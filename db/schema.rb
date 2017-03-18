387199fbc8fd17ef9dff77ae327c0d8623d2e028
robinski <robinskilling@gmail.com>
2017-03-18 22:02:53 +0100

    add featured column to locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211145426) do
+ActiveRecord::Schema.define(version: 20170318210041) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20161211145426) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "featured"
   end
 
   create_table "photos", force: :cascade do |t|
